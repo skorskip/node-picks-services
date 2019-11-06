21a5bfac03976c4d34273e05209c1c5447605ea0
skorskip <skorskip@umich.edu>
2019-11-05 22:07:11 -0500

    Serverless Test

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
+var serverless = require('serverless-http');
 var express = require('express');
 var bodyParser = require('body-parser');
-var config = require('./config.json');
 var cors = require('cors');
 var app = express();
-var port = process.env.PORT || 3000;
 
 app.use(cors());
-app.listen(port);
-
-console.log('API server started on: ' + port);
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var routes = require('./app/routes/routes'); //importing route
-routes(app); //register the route
+
+module.exports.handler = serverless(routes(app));
