d095e9357f5b5440c1a48c3e36eaa05af4eaed53
skorskip <skorskip@umich.edu>
2019-11-19 21:16:29 -0500

    Added current time and quarter

--- app/model/gameModel.js
+++ app/model/gameModel.js
@@ -2,20 +2,22 @@
 var sql = require('./db.js');
 
 var Game = function(game){
-    this.game_id                = game.game_id;
-    this.season                 = game.season;
-    this.week                   = game.week;
-    this.season_game_nbr        = game.season_game_nbr;
-    this.api_game_id            = game.api_game_id;
-    this.start_time             = game.start_time;
-    this.away_team              = game.away_team;
-    this.home_team              = game.home_team;
-    this.home_spread            = game.home_spread;
-    this.game_status            = game.game_status;
-    this.away_team_score        = game.away_team_score;
-    this.home_team_score        = game.home_team_score;
-    this.winning_team           = game.winning_team;
-    this.pick_submit_by_date    = game.submit_by_date;
+    this.game_id                    = game.game_id;
+    this.season                     = game.season;
+    this.week                       = game.week;
+    this.season_game_nbr            = game.season_game_nbr;
+    this.api_game_id                = game.api_game_id;
+    this.start_time                 = game.start_time;
+    this.away_team                  = game.away_team;
+    this.home_team                  = game.home_team;
+    this.home_spread                = game.home_spread;
+    this.game_status                = game.game_status;
+    this.away_team_score            = game.away_team_score;
+    this.home_team_score            = game.home_team_score;
+    this.winning_team               = game.winning_team;
+    this.pick_submit_by_date        = game.submit_by_date;
+    this.current_quarter            = game.current_quarter;
+    this.seconds_left_in_quarter    = game.seconds_left_in_quarter;
 };
 
 Game.addGame = function addGame(newGame, result) {    
@@ -26,7 +28,7 @@ Game.addGame = function addGame(newGame, result) {
 };
 
 Game.getGameById = function getGameById(gameId, result) {
-    sql.query("Select * from games where game_id = ? ORDER BY pick_submit_by_date ASC", gameId, function (err, res) {             
+    sql.query("SELECT * from games where game_id = ? ORDER BY pick_submit_by_date ASC", gameId, function (err, res) {             
         if(err)result(err, null);
         else result(null, res);
     });   
